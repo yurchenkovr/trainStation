19817fab85d5c7e5213387009d3259856b37e08f
Maoltr <mmtretiak@gmail.com>
2018-10-03 23:46:21 +0300

    Added script for creating users table

--- src/main/resources/createDBandTables.sql
+++ src/main/resources/createDBandTables.sql
@@ -11,16 +11,23 @@ CREATE  TABLE IF NOT EXISTS trainstation.trains (
   PRIMARY KEY (TrainNumber) );
 
 CREATE  TABLE IF NOT EXISTS trainstation.Prices (
-  TrainNumber INT INT NOT NULL AUTO_INCREMENT,
+  TrainNumber INT NOT NULL AUTO_INCREMENT,
   TypeCar VARCHAR(45),
   price INT ,
   PRIMARY KEY (TrainNumber) );
 
-CREATE TABLE IF NOT EXISTS trainstation.Workers(
-  id INT NOT NULL AUTO_INCREMENT ,
-  Name VARCHAR (15),
-  SurName VARCHAR (20),
-  Age INT,
-  Salary INT,
-  Position VARCHAR (20)
-  PRIMARY KEY (id) );
+CREATE TABLE IF NOT EXISTS trainstation.Workers (
+  id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Name     VARCHAR(15),
+  SurName  VARCHAR(20),
+  Age      INT,
+  Salary   INT,
+  Position VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS trainstation.Users(
+  id INT AUTO_INCREMENT PRIMARY KEY ,
+  username VARCHAR (15),
+  password VARCHAR (20),
+  role VARCHAR(1)
+);
